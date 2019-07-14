33556f46dd8c9d386da97381d29855f6f9d51478
suhas <suhas.rnr@gmail.com>
2019-07-14 10:51:00 +0530

    Introduced Logout Page Object

---
+++ Logout.py
@@ -0,0 +1,15 @@
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+class LogoutPage:
+	def __init__(self, driver):
+		self.driver = driver
+	def execute(self):
+		welcomeElement = self.driver.find_element_by_id("welcome")
+		ac = ActionChains(self.driver)
+		ac.move_to_element(welcomeElement)
+		ac.click().perform()
+		tenSecWait = WebDriverWait(self.driver,10)
+		logoutElement = tenSecWait.until(EC.presence_of_element_located((By.LINK_TEXT,"Logout")))
+		logoutElement.click()
--- PageObjectModel.py
+++ PageObjectModel.py
@@ -1,19 +1,11 @@
 from selenium import webdriver
 from Login import LoginPage
-from selenium.webdriver.common.action_chains import ActionChains
-from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+from Logout import LogoutPage
 import time
 driver = webdriver.Firefox(executable_path="/Users/Shared/WebDrivers/geckodriver")
 driver.get("https://opensource-demo.orangehrmlive.com/")
 signIn = LoginPage(driver, "Admin", "admin123")
 signIn.execute()
 time.sleep(5)
-welcomeElement = driver.find_element_by_id("welcome")
-ac = ActionChains(driver)
-ac.move_to_element(welcomeElement)
-ac.click().perform()
-tenSecWait = WebDriverWait(driver,10)
-logoutElement = tenSecWait.until(EC.presence_of_element_located((By.LINK_TEXT,"Logout")))
-logoutElement.click()
+signOut = LogoutPage(driver)
+signOut.execute()
