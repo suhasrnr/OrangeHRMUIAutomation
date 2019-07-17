2bf7f13e965098dac12d3f34b78091e714918197
suhas <suhas.rnr@gmail.com>
2019-07-17 21:46:30 +0530

    Issue with welcome element click resolved by time.sleep()

--- Logout.py
+++ Logout.py
@@ -2,11 +2,13 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+import time
 class LogoutPage:
 	def __init__(self, driver):
 		self.driver = driver
 	def execute(self):
 		welcomeElement = self.driver.find_element_by_id("welcome")
+		time.sleep(5)
 		ac = ActionChains(self.driver)
 		ac.move_to_element(welcomeElement)
 		ac.click().perform()
